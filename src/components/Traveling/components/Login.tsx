b18db62edf6f3ca85a707d6a97463e3cfa6e2272
MachineTheSlayer <dangodggw@gmail.com>
2026-01-05 00:21:10 +0400

    add firebase

--- src/components/Traveling/components/Login.tsx
+++ src/components/Traveling/components/Login.tsx
@@ -1,3 +1,6 @@
+import { LockOutlined, UserOutlined } from "@ant-design/icons"
+import { Button, Checkbox, Flex, Form, Input } from "antd"
+
 import styles from "./Login.module.css"
 
 /* export interface ILoginProps {
@@ -14,8 +17,49 @@ const Login: React.FC = () => {
   return (
     <div className={styles.container}>
       <div className={styles.column}>
-        <button>Регистрация</button>
-        <button>Войти</button>
+        <Form
+          name="login"
+          initialValues={{ remember: true }}
+          style={{ maxWidth: 360 }}
+        >
+          <Form.Item
+            name="username"
+            rules={[{ required: true, message: "Please input your Username!" }]}
+          >
+            <Input prefix={<UserOutlined />} placeholder="Username" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: "Please input your Password!" }]}
+          >
+            <Input
+              prefix={<LockOutlined />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Flex justify="space-between" align="center">
+              <Form.Item name="remember" valuePropName="checked" noStyle>
+                <Checkbox>Remember me</Checkbox>
+              </Form.Item>
+              <a href="">Forgot password</a>
+            </Flex>
+          </Form.Item>
+
+          <Form.Item>
+            <Button
+              color="default"
+              variant="solid"
+              block
+              type="primary"
+              htmlType="submit"
+            >
+              Log in
+            </Button>
+            or <a href="">Register now!</a>
+          </Form.Item>
+        </Form>
       </div>
     </div>
   )
